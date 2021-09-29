30f353a2ad1a1ac28ca7dc45274577f55626e622
d3ntastic <36157846+d3ntastic@users.noreply.github.com>
2021-09-29 10:21:37 +0200

    first commit

---
+++ pages/api/cache/purge.js
@@ -0,0 +1,65 @@
+/* eslint-disable no-console */
+import { createHmac } from 'crypto'
+import fetch from 'cross-fetch'
+import { languages } from '../../../services/languageService'
+
+const verifySignature = (signature, body) => {
+	const [rawSign, rawEnv, rawTimestamp] = signature.split(', ')
+
+	const sign = rawSign.replace('sign=', '')
+	const environmentName = rawEnv.replace('env=', '')
+	const timestamp = parseInt(rawTimestamp.replace('t=', ''), 10)
+	const secret = process.env.GRAPHCMS_WEBHOOK_SECRET
+	const payload = JSON.stringify({
+		Body: JSON.stringify(body),
+		environmentName,
+		TimeStamp: timestamp,
+	})
+
+	const hash = createHmac('sha256', secret).update(payload).digest('base64')
+
+	return sign === hash
+}
+
+export default async function handler(req, res) {
+	const {
+		headers: {
+			host,
+		},
+		body,
+	} = req
+
+	const signature = req.headers['gcms-signature']
+	if (signature) {
+		const isValid = verifySignature(signature, body)
+
+		if (isValid) {
+			const isoLanguages = languages.values.reduce((acc, language) => {
+				const { iso, code } = language
+				return { ...acc, [iso]: code }
+			}, {})
+
+			const handledUrls = []
+			body.data.localizations.forEach(async ({ locale, urlIdentifier }) => {
+				const pageUrl = `//${host}/${isoLanguages[locale]}/${urlIdentifier}?purge=1&secret=${process.env.GRAPHCMS_WEBHOOK_SECRET}`
+				try {
+					handledUrls.push(pageUrl)
+					console.log(pageUrl)
+				} catch (err) {
+					console.error(err)
+				}
+			})
+			res.status(200).send(
+				JSON.stringify(handledUrls),
+			)
+		} else {
+			res.status(401).send(
+				'Not authorized',
+			)
+		}
+	} else {
+		res.status(401).send(
+			'Not authorized',
+		)
+	}
+}
