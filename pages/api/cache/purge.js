35596a7b8226e972c8f26079446c88feb615537d
d3ntastic <36157846+d3ntastic@users.noreply.github.com>
2021-09-29 13:13:11 +0200

    lan2

--- pages/api/cache/purge.js
+++ pages/api/cache/purge.js
@@ -43,7 +43,7 @@ export default async function handler(req, res) {
 				method: 'POST',
 				headers: { 'Content-Type':'application/json' },
 				
-				body: JSON.stringify(languages.values),
+				body: JSON.stringify(languages),
 			})
 			const isoLanguages = languages.values.reduce((acc, language) => {
 				const { iso, code } = language
